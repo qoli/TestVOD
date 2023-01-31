82d4b9a2b03ed6168de0d68ba442c35315ad3206
Ronnie W <llqoli@gmail.com>
2023-02-01 04:43:48 +0800

    Initial Commit

---
+++ TestVOD/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  TestVOD
+//
+//  Created by 黃佁媛 on 2023/2/1.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TestVOD/TestVODApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestVODApp.swift
+//  TestVOD
+//
+//  Created by 黃佁媛 on 2023/2/1.
+//
+
+import SwiftUI
+
+@main
+struct TestVODApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
